fc536f8a057a5adf6474b581f6d3166efd0731ce
John Wass <wassj@ctc.com>
2021-05-05 09:32:19 -0400

    add clap

--- README.md
+++ README.md
@@ -3,4 +3,15 @@ an experiment in stig scans
 
 1. probably not a good idea
 2. continue on
-3. ...
+3.
+    ```
+    $ target/debug/k8s-stig 
+    ✅ CNTR-K8-000220 pass
+    ❌ CNTR-K8-002620 basic-auth-file is set
+    Error: "1 failures"
+    $ echo $?
+    1
+    ```
+
+### expect
+- to be using microk8s
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+use clap::Clap;
+
 use rules::*;
 
 mod rules;
 
+#[derive(Clap)]
+struct Opts {
+    /// microk8s current dir
+    #[clap(long, default_value = "/var/snap/microk8s/current")]
+    current_dir: String
+}
+
 fn main() -> Result<(), String> {
-    let cfg = RuleConf { wd: "/var/snap/microk8s/current".into() };
+    let opts = Opts::parse();
+
+    let cfg = RuleConf { wd: opts.current_dir };
     let all: Vec<Box<dyn Rule<ErrType=String>>> = vec![Box::new(K220(&cfg)), Box::new(K2620(&cfg))];
 
     let mut failures = 0;
     for rule in all {
         match rule.check() {
             Ok(_) => {
-                println!("\u{2714} {} pass", rule.id());
+                println!("\u{2705} {} pass", rule.id());
             }
             Err(e) => {
                 println!("\u{274C} {} {}", rule.id(), e);
